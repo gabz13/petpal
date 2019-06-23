d0c47c5ffeae23230d43974baa7d24089dda599f
gabrielle simon <gabriellesimon23@gmail.com>
2019-06-23 15:59:52 +0300

    Animal model

---
+++ app/models/animal.rb
@@ -0,0 +1,13 @@
+class Animal < ApplicationRecord
+  belongs_to :user
+
+  TYPES = ["dog", "cat", "turtle", "bird", "iguana", "hamster", "horse"].freeze
+  SIZES = ["small", "medium", "big", "extra-big"].freeze
+  ENERGY = ["low", "medium", "high", "hyper-active"].freeze
+
+  validates :type, inclusion: { in: TYPES }, presence: true
+  validates :size, inclusion: { in: SIZES }, presence: true
+  validates :energy, inclusion: { in: ENERGY }, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_123339) do
+ActiveRecord::Schema.define(version: 2019_06_23_124730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "animals", force: :cascade do |t|
+    t.string "type"
+    t.string "name"
+    t.text "description"
+    t.string "size"
+    t.string "energy"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_animals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +42,5 @@ ActiveRecord::Schema.define(version: 2019_06_23_123339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "animals", "users"
 end
