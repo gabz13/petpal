1946754f9f443a7d2c4b78a01d6d5326be876a13
gabrielle simon <gabriellesimon23@gmail.com>
2019-06-26 10:32:10 +0300

    columns added to bookings and animals

---
+++ db/migrate/20190626073005_add_location_to_animals.rb
@@ -0,0 +1,6 @@
+class AddLocationToAnimals < ActiveRecord::Migration[5.2]
+  def change
+    add_column :animals, :latitude, :float
+    add_column :animals, :longitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_121135) do
+ActiveRecord::Schema.define(version: 2019_06_26_073005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2019_06_24_121135) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
 
@@ -34,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_06_24_121135) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "terms_and_conditions", default: false
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.index ["animal_id"], name: "index_bookings_on_animal_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
